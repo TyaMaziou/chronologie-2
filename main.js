57664dc7203fbd7ed3889ac7479c82f7477ef986
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2023-08-16 20:29:30 +0200

    essais de reparation du bordel

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 5000,
+      delay: 7000,
       disableOnInteraction: false,
     },
     pagination: {
