c1e0d47ea141c1954df31f3f7a95f9a433381b5d
TyaMaziou <125308500+TyaMaziou@users.noreply.github.com>
2023-08-20 22:43:17 +0200

    continuation du site
    footer/ copyright/ debut deresponsive je continue dans 3 jours

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+
+// header sahdow
+
+let header = document.querySelector('header') ;
+
+window.addEventListener('scroll', () =>{
+  header.classList.toggle('shadow', window.scrollY >0);
+});
+
+
+
+
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -14,53 +26,7 @@ var swiper = new Swiper(".home", {
 
 
 // swiper
-var swiper = new Swiper(".coming-container", {
-  slidesPerView:1,
-  spaceBetween: 10,
-  autoplay: {
-    delay: 5500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints:{
-
-    280:{
-      slidesPerView:1,
-      spaceBetween: 10,
-    },
-
-    320:{
-      slidesPerView:2,
-      spaceBetween: 10,
-    },
-
-    510:{
-      slidesPerView:2,
-      spaceBetween: 10,
-    },
-
-    758:{
-      slidesPerView:3,
-      spaceBetween: 15,
-    },
-
-    900:{
-      slidesPerView:4,
-      spaceBetween: 20,
-    },
-
-  },
-});
-
-// // swiper
-// var swiper = new Swiper(".coming-container-1", {
+// var swiper = new Swiper(".coming-container", {
 //   slidesPerView:1,
 //   spaceBetween: 10,
 //   autoplay: {
@@ -76,6 +42,7 @@ var swiper = new Swiper(".coming-container", {
 //     prevEl: ".swiper-button-prev",
 //   },
 //   breakpoints:{
+
 //     280:{
 //       slidesPerView:1,
 //       spaceBetween: 10,
@@ -100,5 +67,40 @@ var swiper = new Swiper(".coming-container", {
 //       slidesPerView:4,
 //       spaceBetween: 20,
 //     },
+
 //   },
 // });
+
+// swiper
+var swiper = new Swiper(".coming-container", {
+  spaceBetween: 15,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0:{
+      slidesPerView:2,
+    },
+
+    580:{
+      slidesPerView:3,
+    },
+
+    768:{
+      slidesPerView:4,
+    },
+
+    968:{
+      slidesPerView:5,
+    },
+  },
+});
